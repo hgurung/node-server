b022056afe33feaea43871861d4df39ab26b084c
Harris Gurung <harris@Harriss-Mac-mini.local>
2018-01-10 13:01:28 +0545

    Initialized new server of node

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+
+const hbs = require('hbs');
+
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine','hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log',log + '\n');
+    next();
+});
+
+// app.use((req, res , next) => {
+//     res.render('maintenance.hbs');
+// });
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('convertToUpperCase', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs',{
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to homepage'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs',{
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad' ,(req , res) => {
+    res.send({
+        error: 'Unable to find any thing'
+    });
+})
+app.listen(3000, () => {
+    console.log('Server is up on 3000 port');
+});
